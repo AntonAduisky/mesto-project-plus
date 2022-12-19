d84a944cfe3eec03cd395693c346e231c732cdd0
AntonAduisky <a.mltw@yandex.ru>
2022-12-19 23:16:27 +0300

    feat: add create user/user login/get current user

--- src/controllers/user.ts
+++ src/controllers/user.ts
@@ -1,15 +1,48 @@
 import { NextFunction, Request, Response } from 'express';
-import { badRequestError, notFoundError } from '../utils/errors';
+import bcrypt from 'bcrypt';
+import jwt from 'jsonwebtoken';
+import { badRequestError, conflictError, notFoundError } from '../utils/errors';
 import UserModel from '../models/user';
+import { ISessionRequestAuth } from '../utils/types';
 
-export const getUsers = (req: Request, res: Response, next: NextFunction) => {
-  UserModel.find({})
+// реализуем функцию создания пользователя
+export const createUser = async (req: Request, res: Response, next: NextFunction) => {
+  const {
+    name, about, avatar, email, password,
+  } = req.body;
+
+  bcrypt.hash(password, 10)
+    .then((hash: string) => UserModel.create({
+      name, about, avatar, email, password: hash,
+    }))
+    .then((user) => res.send({
+      data: {
+        _id: user._id,
+        name: user.name,
+        about: user.about,
+        avatar: user.avatar,
+        email: user.email,
+      },
+    }))
+    .catch((err) => {
+      if (err.name === 'ValidationError') {
+        return next(badRequestError(err.message));
+      }
+      if (err.code === 11000) {
+        return next(conflictError(err.message));
+      }
+      return next(err);
+    });
+};
+
+export const getUsers = async (req: Request, res: Response, next: NextFunction) => {
+  await UserModel.find({})
     .then((users) => res.send({ data: users }))
     .catch(next);
 };
 
-export const getUserById = (req: Request, res: Response, next: NextFunction) => {
-  UserModel.findById(req.params.userId)
+export const getUserById = async (req: Request, res: Response, next: NextFunction) => {
+  await UserModel.findById(req.params.userId)
     .then((user) => {
       if (!user) {
         throw notFoundError('Пользователь не найден');
@@ -24,25 +57,31 @@ export const getUserById = (req: Request, res: Response, next: NextFunction) => 
     });
 };
 
-// реализуем функцию создания пользователя
-export const createUser = (req: Request, res: Response, next: NextFunction) => {
-  const { name, about, avatar } = req.body;
+export const getCurrentUser = async (
+  req: ISessionRequestAuth,
+  res: Response,
+  next: NextFunction,
+) => {
+  const userId = req.user?._id;
 
-  UserModel.create({ name, about, avatar })
+  UserModel.findById(userId)
+    .orFail(() => notFoundError('Пользователь не найден'))
     .then((user) => res.status(200).send({ data: user }))
     .catch((err) => {
+      if (err.name === 'CastError') {
+        return next(notFoundError('Пользователь не найден'));
+      }
       if (err.name === 'ValidationError') {
         return next(badRequestError(err.message));
       }
-
       return next(err);
     });
 };
 
-export const updateUser = (req: Request, res: Response, next: NextFunction) => {
+export const updateUser = async (req: ISessionRequestAuth, res: Response, next: NextFunction) => {
   const userId = req.user?._id;
 
-  UserModel.findByIdAndUpdate(
+  await UserModel.findByIdAndUpdate(
     userId,
     req.body,
     { new: true, runValidators: true },
@@ -53,15 +92,26 @@ export const updateUser = (req: Request, res: Response, next: NextFunction) => {
     .catch(next);
 };
 
-export const updateAvatar = (req: Request, res: Response, next: NextFunction) => {
+export const updateAvatar = async (req: ISessionRequestAuth, res: Response, next: NextFunction) => {
   const userId = req.user?._id;
 
-  UserModel.findByIdAndUpdate(
+  await UserModel.findByIdAndUpdate(
     userId,
     { avatar: req.body.avatar },
     { new: true, runValidators: true },
   )
     .orFail(() => notFoundError('Пользователь не найден'))
     .then((user) => res.send(user))
+    .catch(next);
+};
+
+export const login = async (req: Request, res: Response, next: NextFunction) => {
+  const { email, password } = req.body;
+
+  await UserModel.findUserByCredentials(email, password)
+    .then((user) => {
+      const token = jwt.sign({ _id: user._id }, 'super-secret-key', { expiresIn: '7d' });
+      res.send({ token });
+    })
     .catch(next);
 };
