72f4234aec412a85a9854bfb5cbd715ef91f71bb
AntonAduisky <a.mltw@yandex.ru>
2022-12-19 23:18:14 +0300

    feat: add card request types

--- src/controllers/card.ts
+++ src/controllers/card.ts
@@ -1,23 +1,24 @@
 import { Request, Response, NextFunction } from 'express';
 import { badRequestError, forbiddenError, notFoundError } from '../utils/errors';
 import CardModel from '../models/card';
+import { ISessionRequestAuth } from '../utils/types';
 // Контроллеры (controllers) содержат основную логику обработки запроса — как правило,
 // набор методов для определённой сущности. Например,
 // для пользователя User это методы создания createUser и авторизации signinUser.
 // В методах описывают, как обрабатывать переданные данные и какой результат возвращать.
 
 export const getCards = async (req: Request, res: Response, next: NextFunction) => {
-  CardModel.find({})
+  await CardModel.find({})
     .then((cards) => res.status(200).send(cards))
     .catch(next);
 };
 
-export const createCard = async (req: Request, res: Response, next: NextFunction) => {
+export const createCard = async (req: ISessionRequestAuth, res: Response, next: NextFunction) => {
   const newCard = {
     ...req.body,
     owner: req.user?._id,
   };
-  CardModel.create(newCard)
+  await CardModel.create(newCard)
     .then((card) => {
       if (!card) {
         // throw генерирует исключение и обработка кода переходит в следующий блок catch
@@ -33,11 +34,11 @@ export const createCard = async (req: Request, res: Response, next: NextFunction
     });
 };
 
-export const deleteCard = async (req: Request, res: Response, next: NextFunction) => {
+export const deleteCard = async (req: ISessionRequestAuth, res: Response, next: NextFunction) => {
   const userId = req.user?._id;
   const { cardId } = req.params;
 
-  CardModel.findById(cardId)
+  await CardModel.findById(cardId)
     .then((card) => {
       if (!card) {
         throw notFoundError('Карточка не найдена');
@@ -56,16 +57,16 @@ export const deleteCard = async (req: Request, res: Response, next: NextFunction
     });
 };
 
-export const addLike = async (req: Request, res: Response, next: NextFunction) => {
-  CardModel.findByIdAndUpdate(
+export const addLike = async (req: ISessionRequestAuth, res: Response, next: NextFunction) => {
+  await CardModel.findByIdAndUpdate(
     req.params.cardId,
-    { $addToSet: { likes: req.user._id } }, // добавить _id в массив, если его там нет
+    { $addToSet: { likes: req.user?._id } }, // добавить _id в массив, если его там нет
     { new: true, runValidators: true },
   )
     .then((card) => {
       if (!card) {
         // throw генерирует исключение и обработка кода переходит в следующий блок catch
-        throw notFoundError('Карточка не найдена');
+        return notFoundError('Карточка не найдена');
       }
       res.send({ data: card });
     })
@@ -79,10 +80,10 @@ export const addLike = async (req: Request, res: Response, next: NextFunction) =
     });
 };
 
-export const removeLike = async (req: Request, res: Response, next: NextFunction) => {
-  CardModel.findByIdAndUpdate(
+export const removeLike = async (req: ISessionRequestAuth, res: Response, next: NextFunction) => {
+  await CardModel.findByIdAndUpdate(
     req.params.cardId,
-    { $pull: { likes: req.user._id } }, // убрать _id из массива
+    { $pull: { likes: req.user?._id } }, // убрать _id из массива
     { new: true, runValidators: true },
   )
     .then((card) => {
